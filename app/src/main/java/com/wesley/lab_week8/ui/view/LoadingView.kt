0e58ec87d30c4b7bfa06a19fda3d18e87f43c737
WesleyGoey <wesley.goey@gmail.com>
2025-10-31 14:01:50 +0700

    loading

--- app/src/main/java/com/wesley/lab_week8/ui/view/LoadingView.kt
+++ app/src/main/java/com/wesley/lab_week8/ui/view/LoadingView.kt
@@ -1,13 +1,39 @@
 package com.wesley.lab_week8.ui.view
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @Composable
 fun LoadingView(
-
+    modifier: Modifier = Modifier
 ) {
-
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .background(Color(0xFF282828)),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = androidx.compose.foundation.layout.Arrangement.Center
+    ) {
+        CircularProgressIndicator(
+            color = Color(0xFFf3bf3b),
+            strokeWidth = 4.dp,
+            modifier = Modifier.size(48.dp)
+        )
+    }
 }
 
 @Preview
